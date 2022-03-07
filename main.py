832a36b757d16726a02996e8fe0ff721ede87b84
UmRange <98126132+UmRange@users.noreply.github.com>
2022-03-06 21:53:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ async def search(ctx, arg1):
     embed.set_thumbnail(url="https://i.pinimg.com/originals/40/55/da/4055da716e80ffef4742b05d40dce139.png")
     embed.add_field(name="Searching File now", value="Results for **"+arg1+"**...", inline=False)
     await ctx.send(embed=embed)
-    search = open("C:/Users/Range/Desktop/test/dumps.txt") #Your path to the file you want the bot to search for
+    search = open("") #Your path to the file you want the bot to search for
     for line in search:
         if arg1 in line:
             embed=discord.Embed(color=0x39fd3f)
